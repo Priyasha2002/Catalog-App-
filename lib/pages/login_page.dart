c7bddd460f3d6e63f3f869ca3884987568be97b6
Priyasha2002 <ghoshpriyasha29@gmail.com>
2023-06-22 12:50:33 +0530

    Init

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset("assets/images/login_page.png",
+          fit: BoxFit.cover,),
+
+          Text("Welcome to Quest Flamingos",style:TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),),
+          SizedBox(height: 20.0,),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical:16.0 , horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter User Name",
+                    labelText: "User Name",
+
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password",
+
+                  ),
+                ),
+                SizedBox(height: 20.0,),
+                ElevatedButton(
+                    child: Text("Login"),
+                  onPressed: () {
+                      print("Logged in successfully");
+                  },
+                )
+              ],
+            ),
+          )
+        ],
+      )
+    );
+
+  }
+
+}
+
